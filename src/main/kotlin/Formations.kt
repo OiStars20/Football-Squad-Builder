06066931d8d218b8ab08083231c2b2bd1f06ffc3
OiStars20 <OiStars20@users.noreply.github.com>
2026-09-23 09:56:12 +0200

    Added all formation

--- src/main/kotlin/Formations.kt
+++ src/main/kotlin/Formations.kt
@@ -1,5 +1,354 @@
 sealed class Formations {
     data class fourThreeThree(val formationPositions: List<Position> = listOf<Position>(
-        Position(Positions.GK,false,0,null)
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.RW,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.LW,false,0,null)
     )) : Formations()
+    data class fourThreeThree2(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.RW,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.LW,false,0,null)
+    )) : Formations()
+    data class fourThreeThree3(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.RW,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.LW,false,0,null)
+    )) : Formations()
+    data class fourThreeThree4(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.RW,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.LW,false,0,null)
+    )) : Formations()
+    data class fourFourOneOne(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class fourFourTwo(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class fourFourTwo2(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class fourFiveOne(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class fourFiveOne2(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class FiveTwoOneTwo(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class FiveTwoThree(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.RW,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.LW,false,0,null)
+    )) : Formations()
+    data class FiveThreeTwo(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class FiveFourOne(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class ThreeOneFourTwo(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class ThreeFourTwoOne(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.CAM,false,0,null)
+    )) : Formations()
+    data class ThreeFourThree(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.RW,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.LW,false,0,null)
+    )) : Formations()
+    data class ThreeFiveTwo(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class FourOneTwoOneTwo(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class FourOneTwoOneTwo2(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class FourOneThreeTwo(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class FourTwoOneThree(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.RW,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.LW,false,0,null)
+    )) : Formations()
+    data class FourTwoTwoTwo(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class FourTwoThreeOne(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class FourTwoThreeOne2(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.CDM,false,0,null),
+        Position(Positions.RM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.LM,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class FourTwoFour(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.RW,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.LW,false,0,null)
+    )) : Formations()
+    data class FourThreeOneTwo(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.ST,false,0,null)
+    )) : Formations()
+    data class FourThreeTwoOne(val formationPositions: List<Position> = listOf<Position>(
+        Position(Positions.GK,false,0,null),
+        Position(Positions.RB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.CB,false,0,null),
+        Position(Positions.LB,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CM,false,0,null),
+        Position(Positions.CAM,false,0,null),
+        Position(Positions.ST,false,0,null),
+        Position(Positions.CAM,false,0,null)
+    )) : Formations()
+
 }
--- src/main/kotlin/Position.kt
+++ src/main/kotlin/Position.kt
@@ -1,5 +1,13 @@
-class Position(positions: Positions, hasPlayer: Boolean, chemistry: Int, player: Player?) {
-    fun setChemistry(){
+class Position(val positions: Positions, var hasPlayer: Boolean, var chemistry: Int, var player: Player?) {
+    fun setPlayer(){
 
+    }
+    fun increaseChemistry(chemistryPlus: Int){
+        if (chemistry + chemistryPlus >= 3){
+            chemistry = 3
+        }
+        else{
+            chemistry = chemistry + chemistryPlus
+        }
     }
 }
